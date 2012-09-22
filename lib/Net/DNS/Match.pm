82a63253524c210459c9e8f98ddd15225b33027e
Wes <github@barely3am.com>
2012-09-22 14:26:22 -0400

    ...

--- lib/Net/DNS/Match.pm
+++ lib/Net/DNS/Match.pm
@@ -30,47 +30,117 @@ our $VERSION = '0.01';
 
 # Preloaded methods go here.
 
+sub new {
+	my $class = shift;
+	my $args = shift;
+	
+	my $self = {};
+	bless($self,$class);
+	
+	return $self;
+}
+
+sub add {
+	my $self = shift;
+	my $array = shift;
+	
+	$array = [ $array ] unless(ref($array) eq 'ARRAY');
+	
+	foreach my $e (@$array){
+		my $first = lc(substr($e,0,1));
+		push(@{$self->{'list'}->{$first}},$e);
+	}
+}
+
+sub match {
+	my $self   = shift;
+	my $things = shift;
+	
+	$things = [ $things ] unless(ref($things) eq 'ARRAY');
+	
+	my $matches;
+	
+	foreach my $thing (@$things){
+	   # first we have to see if this exact domain is in the list
+	   if($self->_match($thing)){
+	   	   push(@$matches,$thing);
+	   } else {  
+		   # if not, we check to see if the subseqent tiered domains
+		   # are in the list somewhere
+		   my @bits = split(/\./,$thing);
+	       my $domain = $bits[$#bits-1].'.'.$bits[$#bits];
+	       if(my $ret = $self->_match($domain)){
+	           push(@$matches,$thing);
+	       }
+
+	   }
+	   
+	}
+	
+	return($matches);
+}
+
+sub _match {
+	my $self = shift;
+	my $thing = shift;
+	
+	my $first = _get_first($thing);
+	return unless(exists($self->{'list'}->{$first}));
+	 
+    my $local_list = $self->{'list'}->{$first};
+    foreach my $m (@$local_list){
+        return 1 if($thing =~ /\.?$m$/);
+    }
+}
+	 
+
+sub _get_first {
+	my $thing = shift;
+	return unless($thing);
+	
+	return lc(substr($thing,0,1));
+}
+	
+
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Net::DNS::Match - Perl extension for blah blah blah
+Net::DNS::Match - Perl extension for testing domains against another list of domains (similar to Net::Patricia but for FQDNs)
 
 =head1 SYNOPSIS
 
   use Net::DNS::Match;
-  blah blah blah
+  use Data::Dumper;
+  my $addr = 'img.yahoo.com';
+
+  my $match = Net::DNS::Match->new();
+  $match->add([
+      'yahoo.com',
+      'google.com',
+      'www.facebook.com',
+   ]);
+ 
+ die Dumper($match->match($addr));
 
 =head1 DESCRIPTION
 
-Stub documentation for Net::DNS::Match, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+This module was initially created to test a list of domains against a whitelist (eg: the Alexa top 1000 list). 
 
 =head2 EXPORT
 
 None by default.
 
-
-
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+collectiveintel.net
 
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+github.com/collectiveintel
 
 =head1 AUTHOR
 
-Wesley Young, E<lt>wes@localE<gt>
+Wesley Young, E<lt>wes@barely3am.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
