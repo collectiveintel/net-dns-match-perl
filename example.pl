23becf8ba177e6b10ca95ce8f8e0689f722976c8
Wes Young <github@barely3am.com>
2013-10-11 12:57:06 -0400

    re-write to use a trie rather than bin search

--- example.pl
+++ example.pl
@@ -7,12 +7,17 @@ use lib './lib';
 
 use Net::DNS::Match;
 use Data::Dumper;
+use Getopt::Std;
 
-my $addr = 'montreal.qc.ca.undernet.org';
-#my $addr = 'zzz.yahoo.com';
+my %opts;
+getopt('dq:',\%opts);
+
+my $query = $opts{'q'} || 'img.yahoo.com';
+my $debug = $opts{'d'};
 
 my $match = Net::DNS::Match->new();
 $match->add([
+    'yahoo.com',
     'abc.yahoo.com',
     'google.com',
     'ca.undernet.org',
@@ -22,9 +27,11 @@ $match->add([
     'ztest2.ca.undernet.org',
     'aaaaol.com',
     'yahoo.com', 
+    'a.yahoo.com',
     'zmg.yahoo.com',
-    
-    
+    'rucl.ru',
+    't.co',
+    'co.uk',
 ]);
  
-die ::Dumper($match->match($addr));
+die ::Dumper($match->match($query));
